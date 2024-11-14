c5808d9ffcfd6d8054a5324fa480f6d5d1e08fba
Manuel Bassi <manuelbassi_8@hotmail.com>
2024-11-14 00:07:07 -0300

    add generateData

--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -7,6 +7,9 @@ import middlewareError from "../middleware/middlewareError.js"
 const router = Router()
 
 const pets = []
+const users = []
+
+
 router.get('/mockingpets', (req, res) => {
     if(pets.length == 0) return res.status(400).json({message:'No pets found'})
     res.status(200).json({payload: pets})
@@ -28,9 +31,13 @@ router.get('/mockingpets/:num', (req, res) => {
     res.status(200).json({payload:pets})
 })
 
+router.get('/mockingusers', (req, res) => {
+    if(users.length == 0) return res.status(400).json({message:'No users found'})
+        res.status(200).json({payload: users})
+})
+
 router.get('/mockingusers/:num', (req, res) => {
     const { num } = req.params
-    const users = []
     if(num <= 0) {
         HandleError.createError({
             name:'Error en parámetro recibido',
@@ -43,6 +50,12 @@ router.get('/mockingusers/:num', (req, res) => {
         users.push(generateUser())
     }
     res.status(200).json({payload:users})
+})
+
+router.post('/generateData/:users/:pets', (req, res) => {
+    const { users, pets } = req.params
+
+    
 })
 
 router.use(middlewareError)
